05b28ba02e81e6c456ced0d2743252b96994c3c0
Lucas Queiroz <lucas.queiroz@fda.ufal.br>
2023-04-11 01:57:03 -0300

    Ajustes alinhamento exames.html desktop

--- assets/js/imagensResponsivas.js
+++ assets/js/imagensResponsivas.js
@@ -36,6 +36,7 @@ function calculateGap() {
         const content = document.querySelector(".hero__content");
         const image = document.querySelector(".hero__image");
         const scheduleSections = document.querySelectorAll(".schedule");
+        const scheduleV2Sections = document.querySelectorAll(".schedulev2");
 
         const containerStyle = window.getComputedStyle(container);
         const containerWidth = parseFloat(containerStyle.width);
@@ -47,14 +48,41 @@ function calculateGap() {
         const imageStyle = window.getComputedStyle(image);
         const imageWidth = parseFloat(imageStyle.width);
 
-        const gap = containerWidth - contentWidth - imageWidth - contentMarginLeft;
+        const scheduleContent = document.querySelector(".schedule__content");
+        let widthDifference = 0;
+
+        if (scheduleContent) {
+            const scheduleContentStyle = window.getComputedStyle(scheduleContent);
+            const scheduleContentWidth = parseFloat(scheduleContentStyle.width);
+            widthDifference = contentWidth - scheduleContentWidth;
+        }
+
+        const gap = containerWidth - contentWidth - imageWidth - contentMarginLeft + widthDifference;
 
         console.log("Gap:", gap);
 
         scheduleSections.forEach((section) => (section.style.gap = `${gap}px`));
+
+        if (scheduleV2Sections.length > 0) {
+            const scheduleV2Title = document.querySelector(".schedulev2__title");
+            let widthDifferenceV2 = 0;
+
+            if (scheduleV2Title) {
+                const scheduleV2TitleStyle = window.getComputedStyle(scheduleV2Title);
+                const scheduleV2TitleWidth = parseFloat(scheduleV2TitleStyle.width);
+                widthDifferenceV2 = contentWidth - scheduleV2TitleWidth;
+            }
+
+            const columnGap = gap + widthDifferenceV2;
+
+            scheduleV2Sections.forEach((section) => (section.style.columnGap = `${columnGap}px`));
+        }
     } else {
         const scheduleSections = document.querySelectorAll(".schedule");
         scheduleSections.forEach((section) => (section.style.gap = ""));
+
+        const scheduleV2Sections = document.querySelectorAll(".schedulev2");
+        scheduleV2Sections.forEach((section) => (section.style.columnGap = ""));
     }
 }
 
